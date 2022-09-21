22eae6ab96c792a093a3c71898bcd13b11e7c2af
Сергей Нестеров <93441101+noradmiral@users.noreply.github.com>
2022-09-22 01:36:46 +0700

    Практика

---
+++ IPL_seminar1-main/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите целое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще одно целое число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+if(firstNum == secondNum * secondNum)
+{
+    Console.WriteLine($"{firstNum}, является квадратом числа {secondNum} ");
+}
+else
+{
+    Console.WriteLine($"{firstNum}, не является квадратом числа {secondNum} ");
+}
---
+++ IPL_seminar1-main/Task08/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(count <= num)
+{
+    if(num % 2 == 0)
+    {
+        Console.WriteLine(num);
+        num++;
+    }
+    else
+    {
+        num++;
+    }
+    count++;
+}
